4d1ec76ee07ce9e50c9d7abc117aa4a37bd6c6ae
IzaelMejia <izaelmejiaa@gmail.com>
2023-09-19 18:07:05 -0600

    sensores actual

--- src/Screens/Sensores.jsx
+++ src/Screens/Sensores.jsx
@@ -1,21 +1,39 @@
-import { StyleSheet, Text, View,Image } from 'react-native'
-import React from 'react'
-import Saludos from '../Components/Saludos'
-import { colors } from '../Global/colors'
-import Premium from '../Components/Premium'
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import axios from 'axios';
+import { colors } from '../Global/colors';
+import Saludos from '../Components/Saludos';
+import Premium from '../Components/Premium';
 
+const Sensores = () => {
+  const [temperatura, setTemperatura] = useState(0);
+  const [humedad, setHumedad] = useState(0);
 
+  // Función para generar valores aleatorios para temperatura y humedad
+  const generarValoresAleatorios = () => {
+    const temperaturaAleatoria = Math.floor(Math.random() * (28 - 18 + 1)) + 18;
+    const humedadAleatoria = Math.floor(Math.random() * (28 - 18 + 1)) + 18;
+    setTemperatura(temperaturaAleatoria);
+    setHumedad(humedadAleatoria);
+  };
 
-const Sensores = ({ navigation }) => {
+  // Actualizar valores aleatorios cada 10 segundos
+  useEffect(() => {
+    const intervalId = setInterval(generarValoresAleatorios, 3000);
+
+    // Limpia el intervalo cuando el componente se desmonta
+    return () => clearInterval(intervalId);
+  }, []);
+  
   return (
     <View style={styles.fondoSensores}>
-      <Saludos/>
-      <Premium/>
-      <View style={styles.contenedorInfoClima}> 
+      <Saludos />
+      <Premium />
+      <View style={styles.contenedorInfoClima}>
         <View style={styles.climaIcono}>
-            <Image
+          <Image
             source={require('../img/sensoresImg/sensor-clima.png')}
             style={styles.imgSensorClima}
             resizeMode="contain"
@@ -23,133 +41,127 @@ const Sensores = ({ navigation }) => {
         </View>
         <View style={styles.contenedorTextoClima}>
           <MaterialIcons name="arrow-drop-up" size={24} color="black" />
-        <View style={styles.contenedorTextoClima2}>
+          <View style={styles.contenedorTextoClima2}>
             <Text style={styles.climaLugar}>Pachuca, Hidalgo</Text>
-            <Text style={styles.climaGrados}>22 C</Text>
+            <Text style={styles.climaGrados}>12 C</Text>
+          </View>
         </View>
-          
-      </View>
       </View>
 
-          {/*-------------- Temperatura de los Sensores --------------*/}
+      {/* Temperatura de los Sensores */}
       <View style={styles.contenedorSensoresTemperatura}>
         <View style={styles.temperatura}>
-          <Text style={styles.textoTemperatura}>28°</Text>
-          <MaterialCommunityIcons name="sun-thermometer" size={35} color={colors.blanco} 
-              style={styles.iconoSolTemperatura}/>
-        </View>
-        
-        <View style={styles.humedad}>
-          <Text style={styles.textoHumedad}>18°</Text>
-          <MaterialCommunityIcons name="sun-thermometer" size={35} color={colors.verdeFuete} 
-                style={styles.iconoSolTemperatura}/>
+          <Text style={styles.textoTemperatura}>{temperatura}°</Text>
+          <MaterialCommunityIcons
+            name="sun-thermometer"
+            size={35}
+            color={colors.blanco}
+            style={styles.iconoSolTemperatura}
+          />
         </View>
 
+        <View style={styles.humedad}>
+          <Text style={styles.textoHumedad}>{humedad}°</Text>
+          <MaterialCommunityIcons
+            name="sun-thermometer"
+            size={35}
+            color={colors.verdeFuerte}
+            style={styles.iconoSolTemperatura}
+          />
+        </View>
       </View>
 
       <View style={styles.ContendedorTextoTemHum}>
-        <View >
-            <Text style={styles.texto}>Temperatura   </Text>
+        <View>
+          <Text style={styles.texto}>Temperatura</Text>
         </View>
-        <View >
-            <Text style={styles.texto}>Humedad   </Text>
+        <View>
+          <Text style={styles.texto}>Humedad</Text>
         </View>
       </View>
 
+      {/* Botón de actualización de datos */}
     </View>
-  )
-}
+  );
+};
 
-export default Sensores
+export default Sensores;
 
 const styles = StyleSheet.create({
-    fondoSensores:{
-        flex:1,
-        backgroundColor:colors.verdePistache
-    },
-    contenedorInfoClima:{
-      marginTop:20,
-      alignItems:"center"
-    },
-    imgSensorClima:{
-      width:130,
-      height:130
-    },
-    contenedorTextoClima:{
-      flexDirection:'row',
-      
+  fondoSensores: {
+    flex: 1,
+    backgroundColor: colors.verdePistache,
   },
-  contenedorTextoClima2:{
-      flexDirection:'column',
-      alignItems:"center",
-      
+  contenedorInfoClima: {
+    marginTop: 20,
+    alignItems: 'center',
   },
-    climaLugar:{
-      fontSize:18,
-      color:colors.verdeFuete
-
+  imgSensorClima: {
+    width: 130,
+    height: 130,
   },
-  climaGrados:{
-      color:"grey",
-      fontSize:12,
-    
+  contenedorTextoClima: {
+    flexDirection: 'row',
   },
-
-  contenedorSensoresTemperatura:{
-    marginTop:30,
-    flexDirection:"row",
-    justifyContent:'space-evenly'
+  contenedorTextoClima2: {
+    flexDirection: 'column',
+    alignItems: 'center',
   },
-
-  temperatura:{
-    width:80,
-    height:200,
-    backgroundColor:colors.verdeFuete,
-    borderRadius:50 
+  climaLugar: {
+    fontSize: 18,
+    color: colors.verdeFuete,
   },
-
-  textoTemperatura:{
-    textAlign:"center",
-    margin:"auto",
-    fontSize:40,
-    padding:2,
-    color:colors.blanco,
-    fontWeight:700,
-    marginVertical:42
+  climaGrados: {
+    color: 'grey',
+    fontSize: 12,
   },
-  iconoSolTemperatura:{
-    textAlign:"center",
-    marginBottom:10
+  contenedorSensoresTemperatura: {
+    marginTop: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
   },
-
-  humedad:{
-    width:80,
-    height:200,
-    backgroundColor:colors.blanco,
-    borderRadius:50 
+  temperatura: {
+    width: 80,
+    height: 200,
+    backgroundColor: colors.verdeFuete,
+    borderRadius: 50,
   },
-
-  textoHumedad:{
-    textAlign:"center",
-    margin:"auto",
-    fontSize:40,
-    padding:2,
-    color:colors.verdeFuete,
-    fontWeight:700,
-    marginVertical:42
+  textoTemperatura: {
+    textAlign: 'center',
+    margin: 'auto',
+    fontSize: 40,
+    padding: 2,
+    color: colors.blanco,
+    fontWeight: 700,
+    marginVertical: 42,
   },
-  ContendedorTextoTemHum:{
-    flexDirection:"row",
-    justifyContent:"space-evenly",
-    textAlign:"center",
-    marginTop:5
+  iconoSolTemperatura: {
+    textAlign: 'center',
+    marginBottom: 10,
   },
-  texto:{
-    color:colors.verdeFuete,
-    fontWeight:700
-  }
-
-  
-
-
-})
+  humedad: {
+    width: 80,
+    height: 200,
+    backgroundColor: colors.blanco,
+    borderRadius: 50,
+  },
+  textoHumedad: {
+    textAlign: 'center',
+    margin: 'auto',
+    fontSize: 40,
+    padding: 2,
+    color: colors.verdeFuete,
+    fontWeight: 700,
+    marginVertical: 42,
+  },
+  ContendedorTextoTemHum: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    textAlign: 'center',
+    marginTop: 5,
+  },
+  texto: {
+    color: colors.verdeFuete,
+    fontWeight: 700,
+  },
+});
